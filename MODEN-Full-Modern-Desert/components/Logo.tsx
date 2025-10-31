35ea6250b384b4f5ccdeb2ae3598007f94cc2657
konstantinostragaris-art <konstantinos.tragaris@gmail.com>
2025-10-31 21:50:07 +0200

    Update Logo.tsx

--- MODEN-Full-Modern-Desert/components/Logo.tsx
+++ MODEN-Full-Modern-Desert/components/Logo.tsx
@@ -10,8 +10,8 @@ export default function Logo({ className }: { className?: string }) {
     <Image
       src={isDark ? '/logo-dark.png' : '/logo-light.png'}
       alt="MODEN Development Logo"
-      width={120}
-      height={120}
+      width={180}
+      height={180}
       priority
       className={className}
     />
