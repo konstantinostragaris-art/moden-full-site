88f6f10f4c77fa8fabf88026e044139dfb77ca00
konstantinostragaris-art <konstantinos.tragaris@gmail.com>
2025-10-31 19:00:57 +0200

    Update next.config.js

--- MODEN-Full-Modern-Desert/next.config.js
+++ MODEN-Full-Modern-Desert/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     typedRoutes: false,
   },
+  i18n: {
+    // Ελληνικά default, Αγγλικά δεύτερη γλώσσα
+    locales: ['el', 'en'],
+    defaultLocale: 'el',
+    localeDetection: true, // προαιρετικό: ανίχνευση browser language
+  },
 };
 
 module.exports = nextConfig;
